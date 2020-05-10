051a41ae6a2bf5d87c19dbee6860dd2dc77bf158
Ash Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2020-05-10 16:02:58 +0100

    A short script to create the additional objects for the joins

---
+++ scripts/week5_additional_objects.R
@@ -0,0 +1,21 @@
+band_instruments3 <- tibble(
+    name =  c("John",   "Paul", "Paul",   "Keith"),
+    plays = c("guitar", "bass", "guitar", "guitar")
+)
+
+band_members2 <- tibble(
+    name = c("Mick",   "John",    "Paul",    "Paul"),
+    band = c("Stones", "Beatles", "Beatles", "Wings")
+)
+
+band_members4 <- tibble(
+    forename = c("Mick",   "John",    "Paul",      "John"),
+    surname =  c("Jagger", "Lennon",  "McCartney", "Squire"),
+    band =     c("Stones", "Beatles", "Beatles",   "Roses")
+)
+
+band_instruments4 <- tibble(
+    forename = c("John",   "Paul",      "Keith",    "John"),
+    surname =  c("Lennon", "McCartney", "Richards", "Squire"),
+    plays =    c("guitar", "bass",      "guitar",   "guitar")
+)
